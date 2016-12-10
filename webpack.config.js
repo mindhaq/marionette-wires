8523189c5f7b9ffc9ac816b9baa957b6bd0ba9e0
Luiz Américo <brandkarl@gmail.com>
2016-12-10 19:11:35 -0300

    Update configuration of css loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {test: /\.hbs$/, loader: 'handlebars'},
       {test: /bootstrap.+\.(jsx|js)$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window' },
       {test: /\.js$/, loader: 'babel?presets[]=es2015', include: [path.resolve(__dirname, 'src')]},
-      {test: /\.less$/, loader: ExtractTextPlugin.extract('css-loader!less-loader')},
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')},
       {test: /\.(woff|woff2)$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       {test: /\.ttf$/,  loader: "url?limit=10000&mimetype=application/octet-stream" },
       {test: /\.eot$/,  loader: "file" },
