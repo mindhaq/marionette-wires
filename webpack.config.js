34dee4d95f310311fd2ee0a226fa45f36de24f8a
Luiz Américo <brandkarl@gmail.com>
2016-12-10 17:17:13 -0300

    Configure less loader to extract into a separate css bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: __dirname + '/src/main.js',
@@ -12,14 +13,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: __dirname + '/dist/index.html',
       template: __dirname + '/src/index.html'
-    })
+    }),
+    // extract inline css into separate 'styles.css'
+    new ExtractTextPlugin(__dirname + '/dist/bundle.css')
   ],
   module: {
     loaders: [
       {test: /\.hbs$/, loader: 'handlebars'},
       {test: /bootstrap.+\.(jsx|js)$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window' },
       {test: /\.js$/, loader: 'babel?presets[]=es2015', include: [path.resolve(__dirname, 'src')]},
-      {test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('css-loader!less-loader')},
       {test: /\.(woff|woff2)$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       {test: /\.ttf$/,  loader: "url?limit=10000&mimetype=application/octet-stream" },
       {test: /\.eot$/,  loader: "file" },
