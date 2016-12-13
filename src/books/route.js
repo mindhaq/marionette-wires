4e4f738859a6ce19641ffc846ce57c9888b0caf1
Luiz Américo <brandkarl@gmail.com>
2016-12-13 17:49:19 -0300

    Remember last visited book route

--- src/books/route.js
+++ src/books/route.js
@@ -3,10 +3,19 @@ import storage from './storage';
 import LayoutView from './layout-view';
 
 export default Route.extend({
-  activate() {
+  activate(transition) {
+    if (this.previousRoute) {
+      transition.redirectTo(this.previousRoute.name, this.previousRoute.params, this.previousRoute.query)
+      return
+    }
     return storage.findAll({ajaxSync: true}).then(collection => {
       this.collection = collection;
     });
+  },
+
+  deactivate(transition) {
+    let prevRoutes = transition.prev.routes;
+    this.previousRoute = prevRoutes[prevRoutes.length - 1]
   },
 
   viewClass: LayoutView,
